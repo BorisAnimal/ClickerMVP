a0835e7f1d0e729833649b71055c59f7d2c451f8
boo <b.guryev@innopolis.ru>
2017-10-10 01:21:32 +0300

    image added

--- app/src/main/java/com/example/boo/hellomvp/ui/ActivityMain.java
+++ app/src/main/java/com/example/boo/hellomvp/ui/ActivityMain.java
@@ -3,7 +3,9 @@ package com.example.boo.hellomvp.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,45 +13,48 @@ import com.example.boo.hellomvp.R;
 import com.example.boo.hellomvp.mvp.CounterMVP;
 import com.example.boo.hellomvp.mvp.presenters.CounterPresenter;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by boo on 26.09.17.
  */
 
 public class ActivityMain extends AppCompatActivity implements CounterMVP.View {
+    /**
+     * week 1
+     */
     private CounterMVP.Presenter presenter;
-    private Button incButton;
-    private Button decButton;
-    private TextView counterView;
+    @BindView(R.id.main_button_inc)
+    Button incButton;
+    @BindView(R.id.main_button_dec)
+    Button decButton;
+    @BindView(R.id.main_counter)
+    TextView counterView;
 
     /**
-     * Week 2 TODO:
-     *
-     * View (представления) и всё что с ними связано:
-     *      XML и пользовательские элементы (кнопки, текст, ввод текста, картинки),
-     *      получение с них информации
-     *
-     *      Size (dp, sp, px, %)
-     *
-     *      Layout (контейнеры)
-     *
-     *      Dialogs (получение с них информации)
-     *
-     *      Advanced: подключить пользовательский элемент с чужого github - кастомный
-     *
-     *      Вы ещё живы? Поговорить про второе Activity, цикл жизни Activity, поворот экрана.
+     * week 2
      */
+    @BindView(R.id.image)
+    ImageView imageView;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /**
+         * week 1
+         */
+
         presenter = new CounterPresenter(this);
 
-        //TODO: Gooogle "Butterknife" to make it simple, clever 
-        incButton = (Button) findViewById(R.id.main_button_inc);
-        decButton = (Button) findViewById(R.id.main_button_dec);
-        counterView = (TextView) findViewById(R.id.main_counter);
+        // Gooogle "Butterknife" to make it simple, clever
+        ButterKnife.bind(this);
+//        incButton = (Button) findViewById(R.id.main_button_inc);
+//        decButton = (Button) findViewById(R.id.main_button_dec);
+//        counterView = (TextView) findViewById(R.id.main_counter);
 
         presenter.setBaseCounter();
 
@@ -66,7 +71,35 @@ public class ActivityMain extends AppCompatActivity implements CounterMVP.View {
                 presenter.increaseCounter();
             }
         });
+
+        /**
+         * week 2
+         */
+        //1. Передача ресурса как аргумент к полю View
+        imageView.setImageResource(R.mipmap.no_image);
+        //2. Разные способы задать обработчик события для View
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                swapImageState(view);
+            }
+        });
+
     }
+
+    /**
+     * week 2
+     */
+
+    public void swapImageState(View view) {
+        if(imageView.getVisibility() == View.VISIBLE)
+            imageView.setVisibility(View.INVISIBLE);
+        else imageView.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * week 1
+     */
 
     @Override
     public void updateCounter(int value) {
@@ -78,4 +111,6 @@ public class ActivityMain extends AppCompatActivity implements CounterMVP.View {
     public void showNotification(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
+
+
 }
